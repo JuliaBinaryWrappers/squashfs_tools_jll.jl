1f2d6d0863016617ade500e55e609732afba7fe6
jlbuild <juliabuildbot@gmail.com>
2020-10-28 16:03:19 +0000

    squashfs_tools_jll build 4.4.0+0

---
+++ src/squashfs_tools_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule squashfs_tools_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("squashfs_tools")
+JLLWrappers.@generate_main_file("squashfs_tools", UUID("eed32e3e-a7c5-5bf9-9121-5cf3ab653887"))
+end  # module squashfs_tools_jll
