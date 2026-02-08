f16077374990b360b27ca17415b2fc088edacafb
jlbuild <juliabuildbot@gmail.com>
2026-02-08 19:21:10 +0000

    squashfs_tools_jll build 4.7.4+0

--- src/squashfs_tools_jll.jl
+++ src/squashfs_tools_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("squashfs_tools")
-JLLWrappers.@generate_main_file("squashfs_tools", UUID("eed32e3e-a7c5-5bf9-9121-5cf3ab653887"))
+JLLWrappers.@generate_main_file("squashfs_tools", Base.UUID("eed32e3e-a7c5-5bf9-9121-5cf3ab653887"))
 end  # module squashfs_tools_jll
